7362a192f20429fc8cbca146bca5dabe9370a480
Daniel Escobar <dany.dim@hotmail.com>
2022-06-20 17:20:03 -0500

    This is the first try of the mobile computation app. The first activity was created, but we were looking on how to scroll.

---
+++ Lab1-UI/src/main/java/com/udea/mobilecomputation/labs20221_gr02/lab1/PersonalDataActivity.kt
@@ -0,0 +1,16 @@
+package com.udea.mobilecomputation.labs20221_gr02.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class PersonalDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+    }
+
+
+
+
+}
