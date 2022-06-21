14e6fa4b9ee370623b08adb89f6f5f20ee91f2fb
Daniel Escobar <dany.dim@hotmail.com>
2022-06-20 21:23:52 -0500

    Second Commit with scrollable working and trying to find out a way to fill the spinner with countries using an API.

---
+++ Lab1-UI/src/main/java/com/udea/mobilecomputation/labs20221_gr02/lab1/ContactDataActivity.kt
@@ -0,0 +1,11 @@
+package com.udea.mobilecomputation.labs20221_gr02.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_data)
+    }
+}
