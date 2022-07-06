cdcfc35ef4b26484da76e1e89104b12afbfdbde3
juan esteban <juan.garciam1@udea.edu.co>
2022-07-05 19:54:15 -0500

    Se valida campos de la segunda pantalla, se imprime por consola la informacion personal, solo falta el grado academico

--- Lab1-UI/src/main/java/com/udea/mobilecomputation/labs20221_gr02/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/com/udea/mobilecomputation/labs20221_gr02/lab1/ContactDataActivity.kt
@@ -1,11 +1,59 @@
 package com.udea.mobilecomputation.labs20221_gr02.lab1
 
+import android.content.ContentValues.TAG
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isNotEmpty
 
 class ContactDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_data)
+        saveContact()
     }
+
+    private fun saveContact() {
+
+        val button: Button = findViewById(R.id.button_next)
+        val phone: EditText = findViewById(R.id.phone_input)
+        val email: EditText = findViewById(R.id.mail_input)
+        val country: Spinner = findViewById(R.id.country_input)
+        val bundle = intent.extras
+        val dataName = bundle?.getString("name")
+        val dataLastName = bundle?.getString("last_name")
+        val dataDate = bundle?.getString("date")
+        val dataGender = bundle?.getString("gender")
+
+        button.setOnClickListener {
+            if (phone.text.isNotEmpty() && email.text.isNotEmpty() && country.isNotEmpty()) {
+               // Log.i(TAG,"El telefono del usuario es: "+phone.text)
+                Log.i(TAG, "El nombre del usuario es: $dataName")
+                Log.i(TAG, "El apellido del usuario es: $dataLastName")
+                Log.i(TAG, "El date del usuario es: $dataDate")
+                Log.i(TAG, "El genero del usuario es: $dataGender")
+            } else {
+                showDialog()
+            }
+        }
+    }
+
+        private fun showDialog(){
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialog)
+            builder.setMessage(R.string.messageContactActivity)
+            builder.setPositiveButton(R.string.positive, DialogInterface.OnClickListener{ dialog, which ->
+                Toast.makeText(this,"Positive Button Action", Toast.LENGTH_LONG).show()
+            })
+            builder.show()
+        }
+
+
+
 }
--- Lab1-UI/src/main/java/com/udea/mobilecomputation/labs20221_gr02/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/com/udea/mobilecomputation/labs20221_gr02/lab1/PersonalDataActivity.kt
@@ -4,18 +4,18 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.get
+import androidx.core.view.isNotEmpty
+import java.util.*
 
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
         goToContactActivity()
+
     }
 
     private fun goToContactActivity() {
@@ -23,10 +23,30 @@ class PersonalDataActivity : AppCompatActivity() {
         val button:Button = findViewById(R.id.button_next)
         val name:EditText = findViewById(R.id.name_input)
         val lastName: EditText = findViewById(R.id.last_name_input)
+        val gender: RadioGroup = findViewById(R.id.gender_group)
         val date: DatePicker = findViewById(R.id.date_input)
+        lateinit var radioButton: RadioButton
+
+
         button.setOnClickListener{
-            if(name.text.isNotEmpty() && lastName.text.isNotEmpty()) {
+            if(name.text.isNotEmpty() && lastName.text.isNotEmpty() && date.isNotEmpty()) {
                 val intent = Intent(this, ContactDataActivity::class.java)
+                intent.putExtra("name", name.text.toString())
+                intent.putExtra("last_name", lastName.text.toString())
+
+
+                var birth_date:String = ""
+                var day = date.dayOfMonth
+                var month = date.month
+                var year = date.year
+
+                birth_date = day.toString()+"/" + month.toString()+"/" + year.toString()
+                intent.putExtra("date", birth_date)
+
+                val selectedOption: Int = gender!!.checkedRadioButtonId
+                radioButton = findViewById(selectedOption)
+
+                intent.putExtra("gender", radioButton.text)
                 startActivity(intent)
             }else{
                 showDialog()
@@ -37,10 +57,11 @@ class PersonalDataActivity : AppCompatActivity() {
     private fun showDialog(){
         val builder = AlertDialog.Builder(this)
         builder.setTitle(R.string.dialog)
-        builder.setMessage(R.string.message)
+        builder.setMessage(R.string.messagePersonalActivity)
         builder.setPositiveButton(R.string.positive, DialogInterface.OnClickListener{dialog, which ->
             Toast.makeText(this,"Positive Button Action", Toast.LENGTH_LONG).show()
         })
         builder.show()
     }
 }
+
